6216fd46d30edd24cbf7bbe9f31573b1634228c9
hjs9605 <hjs9605@naver.com>
2022-05-06 11:28:12 +0900

    jisoo commit

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SignUpPage from "./components/signup/signup";
+import LogInPage from "./components/login/login";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/login" element={<LogInPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
