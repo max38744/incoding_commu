3fe53a3de2e7be6a572af70bd559338b30d17f15
hjs9605 <hjs9605@naver.com>
2022-05-15 17:48:54 +0900

    forgot page 작업중

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SignUpPage from "./components/signup/signup";
 import LogInPage from "./components/login/login";
+import ForgotPage from "./components/forgot/forgot"
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUpPage/>} />
           <Route path="/login" element={<LogInPage/>} />
+          <Route path="/forgot" element={<ForgotPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
