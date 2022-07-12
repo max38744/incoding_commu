bf4e799dc628e4a793f09659bc51583f98330386
hjs9605 <hjs9605@naver.com>
2022-07-13 08:59:24 +0900

    styled

---
+++ client/client/src/components/forgot/IdForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import TextField from '@mui/material/TextField';
+
+import styled from "styled-components";
+const Namezone = styled.div`
+    font-size : 1.2rem;
+`;
+
+const Phonezone = styled.div`
+    font-size : 1.2rem;
+    margin-top : 10px;
+`;
+
+const FindId = styled.div`
+    display: flex;
+    justify-content: left;
+`;
+
+export default function IdForm() {
+
+    return (
+        <FindId>
+            <form>
+                <Namezone>
+                    이름 : ㅎㅎ
+
+
+                <input id="Name" label="Name" variant="Name" />
+                </Namezone>
+                <Phonezone>
+                    휴대폰 번호:
+                <input id="Phone" label="Phone" variant="Phone" />
+                </Phonezone>
+            </form>
+        </FindId>
+    )
+}
